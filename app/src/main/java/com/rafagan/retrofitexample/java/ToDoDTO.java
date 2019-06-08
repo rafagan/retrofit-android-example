16e33ac35049e824d6b5362c9c7bbbf9ae1ce5be
Ráfagan <rafagansa@outlook.com>
2019-06-08 19:15:11 -0300

    ToDoDTO in java

--- app/src/main/java/com/rafagan/retrofitexample/java/ToDoDTO.java
+++ app/src/main/java/com/rafagan/retrofitexample/java/ToDoDTO.java
@@ -1,4 +1,50 @@
 package com.rafagan.retrofitexample.java;
 
 public class ToDoDTO {
+    private String userId;
+    private String id;
+    private String title;
+    private String completed;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(String completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoDTO{" +
+                "userId='" + userId + '\'' +
+                ", id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", completed='" + completed + '\'' +
+                '}';
+    }
 }
