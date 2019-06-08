922d336df1631afa3562f5691db7f73119ba87ba
Ráfagan <rafagansa@outlook.com>
2019-06-08 19:17:15 -0300

    ToDoAPI in java

--- app/src/main/java/com/rafagan/retrofitexample/java/ToDoAPI.java
+++ app/src/main/java/com/rafagan/retrofitexample/java/ToDoAPI.java
@@ -1,4 +1,11 @@
 package com.rafagan.retrofitexample.java;
 
-public class ToDoAPI {
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import java.util.List;
+
+public interface ToDoAPI {
+    @GET("todos")
+    Call<List<ToDoDTO>> getToDos();
 }
