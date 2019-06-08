cbe1110c8177df17724dd3891bbf0e1ddb6cbcc3
Ráfagan <rafagansa@outlook.com>
2019-06-08 19:26:41 -0300

    First part of ToDoService in java

--- app/src/main/java/com/rafagan/retrofitexample/java/ToDoService.java
+++ app/src/main/java/com/rafagan/retrofitexample/java/ToDoService.java
@@ -1,4 +1,28 @@
 package com.rafagan.retrofitexample.java;
 
-public class ToDoService {
+import okhttp3.ResponseBody;
+
+import java.util.List;
+
+public class ToDoService extends BaseService<ToDoAPI> {
+    public interface OnSuccess {
+        void run(List<ToDoDTO> toDos);
+    }
+
+    public interface OnError {
+        void run(ResponseBody errorBody);
+    }
+
+    public ToDoService(Class<ToDoAPI> toDoAPIClass) {
+        super(toDoAPIClass);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return "https://jsonplaceholder.typicode.com/";
+    }
+
+    public void getToDos(OnSuccess onSuccess, OnError onError) {
+
+    }
 }
